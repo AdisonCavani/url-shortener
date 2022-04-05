de34c9cd19bd949585887c9df61e5ecb251dea15
Adison Cavani <adriansrodon@tuta.io>
2022-04-05 15:08:28 +0200

    Move configuring ElasticSearch to extension method

--- src/UrlShortener/Logging/SerilogExtensions.cs
+++ src/UrlShortener/Logging/SerilogExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
-namespace UrlShortener;
+namespace UrlShortener.Logging;
 
 public static class SerilogExtensions
 {
@@ -14,29 +13,14 @@ public static class SerilogExtensions
     {
         builder.UseSerilog((context, configuration) =>
         {
-            configuration.Enrich.FromLogContext()
+            configuration
+                .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
 #if DEBUG
                 .WriteTo.Console()
                 .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "logs", "log.txt"), rollingInterval: RollingInterval.Day)
 #endif
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-                {
-                    IndexFormat = $"{context.Configuration["AppName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace('.', '-')}-{DateTime.UtcNow:yyyy-MM}",
-                    AutoRegisterTemplate = true,
-                    NumberOfShards = 2,
-                    NumberOfReplicas = 1,
-
-                    // Handling errors
-                    /*
-                    FailureCallback = e => Log.Error("ES was unable to submit event " + e.MessageTemplate),
-                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-                                       EmitEventFailureHandling.WriteToFailureSink |
-                                       EmitEventFailureHandling.RaiseCallback,
-                    FailureSink = new FileSink(Path.Combine(AppContext.BaseDirectory, "logs", "ESfailures.txt"), new JsonFormatter(), null)
-                    */
-
-                })
+                .UseElasticSearch(context)
                 .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
                 .ReadFrom.Configuration(context.Configuration);
         });
