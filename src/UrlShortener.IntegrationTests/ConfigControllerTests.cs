cd00164a770bf3c2c0816d5297521fec4bf8b6bd
Adison Cavani <adriansrodon@tuta.io>
2022-05-01 23:07:44 +0200

    Update AwsParameterStore to work in test env
    TODO: fix env variables for testing env

---
+++ src/UrlShortener.IntegrationTests/ConfigControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UrlShortener.Contracts.V1;
+using UrlShortener.Models.Responses;
+using Xunit;
+
+namespace UrlShortener.IntegrationTests;
+
+public class ConfigControllerTests : IntegrationTest
+{
+    public ConfigControllerTests() : base("https://localhost:7081/", "v1")
+    {
+
+    }
+
+    [Fact]
+    public async Task Get_WhenBearerIsMissing_ReturnsUnauthorized()
+    {
+        // Arrange
+
+        // Act
+        var response = await TestClient.GetAsync(GetRoute(ApiRoutes.Config.Get));
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Get_WhenLoggedIn_ReturnsOk()
+    {
+        // Arrange
+        await AuthenticateAsync();
+
+        // Act
+        var response = await TestClient.GetAsync(GetRoute(ApiRoutes.Config.Get));
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var responseObj = await response.Content.ReadAsAsync<SettingsDto>();
+        Assert.NotNull(responseObj);
+    }
+}
--- src/UrlShortener/Extensions/AwsParameterStore.cs
+++ src/UrlShortener/Extensions/AwsParameterStore.cs
@@ -8,10 +8,10 @@ public static class AwsParameterStore
         {
             confBuilder.AddSystemsManager(config =>
             {
-                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.ToLower();
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?.ToLower(); // FIX: this will not work in test env
 
                 // Parameter Store prefix to pull configuration data from.
-                config.Path = $"/{environment}/url-shortener";
+                config.Path = $"/{environment ?? "development"}/url-shortener";
 
                 // Reload configuration data every 15 minutes.
                 config.ReloadAfter = TimeSpan.FromMinutes(15);
