6ef2ac18cfc8a786fc6d8d13f7430c11d0181ebb
Adison Cavani <adriansrodon@tuta.io>
2022-06-27 11:15:01 +0200

    Fix auth error in Config IntegrationTest

--- src/UrlShortener.IntegrationTests/AuthHelpers.cs
+++ src/UrlShortener.IntegrationTests/AuthHelpers.cs
@@ -8,24 +8,25 @@ namespace UrlShortener.IntegrationTests;
 
 public static class AuthHelpers
 {
-    public static async Task<HttpClient> AuthenticateAsync(this HttpClient client, RouteResolver route)
+    public static async Task AuthenticateAsync(this HttpClient client, RouteResolver route)
     {
+        const string email = "test@email.com";
+        const string password = "Password123!";
+        
         var register = await client.PostAsJsonAsync(route.Get(ApiRoutes.Account.Register), new RegisterUserDto()
         {
-            Email = "test@email.com",
-            Password = "password"
+            Email = email,
+            Password = password
         });
 
         var login = await client.PostAsJsonAsync(route.Get(ApiRoutes.Account.Login), new LoginDto()
         {
-            Email = "test@email.com",
-            Password = "password"
+            Email = email,
+            Password = password
         });
 
         var token = await login.Content.ReadAsStringAsync();
 
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
-
-        return client;
     }
 }
