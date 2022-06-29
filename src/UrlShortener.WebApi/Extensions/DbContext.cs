68983a33876746b8ad223016ebb4488c9de94470
Adison Cavani <adriansrodon@tuta.io>
2022-06-29 10:05:07 +0200

    Change db provider to PostgreSQL

--- src/UrlShortener.WebApi/Extensions/DbContext.cs
+++ src/UrlShortener.WebApi/Extensions/DbContext.cs
@@ -8,7 +8,7 @@ public static class DbContext
     public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(configuration["AppSettings:SqlConnection"], sqlOptions =>
+            options.UseNpgsql(configuration["AppSettings:SqlConnectionString"], sqlOptions =>
             {
                 sqlOptions.EnableRetryOnFailure(10, TimeSpan.FromSeconds(30), null);
             }));
