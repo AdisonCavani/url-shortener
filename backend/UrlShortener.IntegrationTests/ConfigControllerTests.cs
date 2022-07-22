50e2ea48635866ea66c40e8a41669fb93928dcf8
Adison Cavani <adriansrodon@tuta.io>
2022-07-22 17:40:37 +0200

    Add tests to api solution

--- backend/UrlShortener.IntegrationTests/ConfigControllerTests.cs
+++ backend/UrlShortener.IntegrationTests/ConfigControllerTests.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
-using UrlShortener.Core.Contracts.V1;
 using UrlShortener.Api;
+using UrlShortener.Shared.Contracts;
 using Xunit;
 
 namespace UrlShortener.IntegrationTests;
--- backend/UrlShortener.IntegrationTests/IntegrationTestWebFactory.cs
+++ backend/UrlShortener.IntegrationTests/IntegrationTestWebFactory.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using UrlShortener.Api.Models.App;
+using UrlShortener.Api.Database;
 
 namespace UrlShortener.IntegrationTests;
 
